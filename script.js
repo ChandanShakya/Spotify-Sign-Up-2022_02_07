34c4b40b176afd46528e2b177e5f0d7c502f915c
ZXY-CC-3ag13 <70548117+ZXY-CC-3ag13@users.noreply.github.com>
2022-02-07 20:06:54 +0000

    Corrected the UI

---
+++ script.js
@@ -0,0 +1,37 @@
+function validate() {
+    var email = document.forms["MainForm"]["email"].value;
+    var confirmEmail = document.forms["MainForm"]["confirmEmail"].value;
+    var password = document.forms["MainForm"]["password"].value;
+    var month = document.getElementsByClassName("month").value;
+    var day = document.getElementsById("day").value;
+    var year = document.getElementsById("year").value;
+
+    var simpleEmailRegex = /\S+@\S+\.\S+/;
+
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    } else if (email != simpleEmailRegex) {
+        alert("Email must be in the format of main@email.com");
+        return false;
+    } else if (confirmEmail == "") {
+        alert("Confirm Email must be filled out");
+        return false;
+    } else if (confirmEmail != email) {
+        alert("Email and Confirm Email must match");
+        return false;
+    } else if (password == "") {
+        alert("Password must be filled out");
+        return false;
+    } else if (month == "") {
+        alert("Month must be selected");
+        return false;
+    } else if (day == "") {
+        alert("Day must be selected");
+        return false;
+    } else if (year == "") {
+        alert("Year must be selected");
+        return false;
+    }
+
+}
