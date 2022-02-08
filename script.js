1e5126d32cff68e54e176b308f5c9c6a7c8ac648
ZXY-CC-3ag13 <zxy-cc-3ag13@protonmail.com>
2022-02-08 13:40:44 +0545

    Improvement : Add capcha verification

--- script.js
+++ script.js
@@ -33,5 +33,13 @@ function validate() {
         alert("Year must be selected");
         return false;
     }
+    if(recaptcha_response.length == 0) {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
 
+}
+function verifyCaptcha(token) {
+    recaptcha_response = token;
+    document.getElementById('g-recaptcha-error').innerHTML = '';
 }
